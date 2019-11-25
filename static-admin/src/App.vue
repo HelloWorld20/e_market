ee5fe4554842d0e8af03f95560e621fc9ed74b01
HelloWorld20 <292791515@qq.com>
2019-11-25 22:45:42 +0800

    commit

--- static-admin/src/App.vue
+++ static-admin/src/App.vue
@@ -10,7 +10,7 @@
                 <el-submenu index="1">
                     <template slot="title">
                         <i class="el-icon-location"></i>
-                        <span>导航一</span>
+                        <span>商品管理</span>
                     </template>
                     <el-menu-item index="1-1" data-id="Category"
                         >分类管理</el-menu-item
@@ -27,11 +27,11 @@
                 </el-submenu>
                 <el-menu-item index="2">
                     <i class="el-icon-menu"></i>
-                    <span slot="title">导航二</span>
+                    <span slot="title">订单管理</span>
                 </el-menu-item>
                 <el-menu-item index="3">
                     <i class="el-icon-document"></i>
-                    <span slot="title">导航三</span>
+                    <span slot="title">用户管理</span>
                 </el-menu-item>
                 <el-menu-item index="4">
                     <i class="el-icon-setting"></i>
