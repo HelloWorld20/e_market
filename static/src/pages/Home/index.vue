6b3e3a95b94563ded1ba3333f1bcdf0eba0245cb
Leon.W <292701515@qq.com>
2020-01-06 15:13:02 +0800

    添加滚动到底部事件

--- static/src/pages/Home/index.vue
+++ static/src/pages/Home/index.vue
@@ -1,65 +1,74 @@
 <template>
-	<div class="home">
-		<van-search
-			placeholder="请输入搜索关键词"
-			v-model="searchVal"
-			show-action
-			shape="round"
-			@search="onSearch"
-			@cancel="onCancel"
-		/>
-		<div class="home-carousel">
-			<van-swipe :autoplay="4000" style="height: 100%">
-				<van-swipe-item
-					class="carousel-item"
-					v-for="(item, index) in carousel"
-					:key="index"
-					:style="`background-image: url(${item})`"
+	<div class="home" ref="wrapper">
+		<div ref="inner">
+			<van-search
+				placeholder="请输入搜索关键词"
+				v-model="searchVal"
+				show-action
+				shape="round"
+				@search="onSearch"
+				@cancel="onCancel"
+			/>
+			<div class="home-carousel">
+				<van-swipe :autoplay="4000" style="height: 100%">
+					<van-swipe-item
+						class="carousel-item"
+						v-for="(item, index) in carousel"
+						:key="index"
+						:style="`background-image: url(${item})`"
+					>
+					</van-swipe-item>
+				</van-swipe>
+			</div>
+			<van-grid :column-num="5" class="home-category">
+				<van-grid-item
+					class="home-category-item"
+					:key="item.id"
+					v-for="item in cateData"
+					@click="handleClickCate(item)"
 				>
-				</van-swipe-item>
-			</van-swipe>
-		</div>
-		<van-grid :column-num="5" class="home-category">
-			<van-grid-item
-				class="home-category-item"
-				:key="item.id"
-				v-for="item in cateData"
-				@click="handleClickCate(item)"
-			>
-				<div
-					class="home-category-item-img"
-					:style="`background-image: url(${getImages(item.images)})`"
-				></div>
-				<div class="home-category-item-name">{{ item.name }}</div>
-			</van-grid-item>
-		</van-grid>
-
-		<van-divider>推荐商品</van-divider>
-
-		<van-grid :column-num="2" class="home-rec">
-			<van-grid-item v-for="(item, index) in recommendData" :key="index">
-				<div class="home-rec-item">
 					<div
-						class="home-rec-item-img"
+						class="home-category-item-img"
 						:style="
 							`background-image: url(${getImages(item.images)})`
 						"
 					></div>
-					<div class="home-rec-item-name">{{ item.name }}</div>
-					<div class="home-rec-item-desc">{{ item.desc }}</div>
-					<div class="home-rec-item-control">
-						<div class="home-rec-item-control-pirce">
-							￥{{ item.prise }}
+					<div class="home-category-item-name">{{ item.name }}</div>
+				</van-grid-item>
+			</van-grid>
+
+			<van-divider>推荐商品</van-divider>
+
+			<van-grid :column-num="2" class="home-rec">
+				<van-grid-item
+					v-for="(item, index) in recommendData"
+					:key="index"
+				>
+					<div class="home-rec-item">
+						<div
+							class="home-rec-item-img"
+							:style="
+								`background-image: url(${getImages(
+									item.images
+								)})`
+							"
+						></div>
+						<div class="home-rec-item-name">{{ item.name }}</div>
+						<div class="home-rec-item-desc">{{ item.desc }}</div>
+						<div class="home-rec-item-control">
+							<div class="home-rec-item-control-pirce">
+								￥{{ item.prise }}
+							</div>
+							<van-button
+								type="primary"
+								icon="cart-o"
+								@click="add2cart(item.id)"
+							></van-button>
 						</div>
-						<van-button
-							type="primary"
-							icon="cart-o"
-							@click="add2cart(item.id)"
-						></van-button>
 					</div>
-				</div>
-			</van-grid-item>
-		</van-grid>
+				</van-grid-item>
+			</van-grid>
+		</div>
 	</div>
 </template>
 
@@ -67,6 +76,8 @@
 import { Swipe, SwipeItem, Grid, GridItem, Search } from 'vant';
 import { mapActions } from 'vuex';
 import { getRecommend, addOrUpdateCart } from '../../http/apis';
+import { createHandleScroll } from '../../common/utils';
+
 export default {
 	data() {
 		return {
@@ -93,6 +104,18 @@ export default {
 		this.carousel = carousel.carousel;
 		this.cateData = category;
 		this.recommendData = recommendData;
+	},
+	mounted() {
+		const elWrapper = this.$refs['wrapper'];
+		const elInner = this.$refs['inner'];
+		createHandleScroll(elWrapper, elInner, () => {
+			getRecommend().then(
+				recommendData =>
+					(this.recommendData = this.recommendData.concat(
+						recommendData
+					))
+			);
+		});
 	},
 	methods: {
 		...mapActions(['getCategory', 'getUserInfo', 'getHomeInfo']),
--- static/src/pages/Me/index.vue
+++ static/src/pages/Me/index.vue
@@ -6,28 +6,45 @@
 				class="me-content-avatar"
 				:style="{ backgroundImage: `url(${userInfo.headimgurl})` }"
 			></div>
-			<div class="me-content-name">{{userInfo.nickName}}</div>
+			<div class="me-content-name">{{ userInfo.nickName }}</div>
 			<div class="me-content-cell">
 				<van-cell title="我的订单" is-link to="/MyOrder"></van-cell>
 				<van-cell title="我的地址" is-link to="/Addr"></van-cell>
-				<van-cell title="关于" is-link></van-cell>
+				<van-cell
+					title="关于"
+					is-link
+					@click="showPopUp = true"
+				></van-cell>
 			</div>
 		</div>
+		<van-popup
+			v-model="showPopUp"
+			round
+			position="bottom"
+			:style="{ height: '20%' }"
+		>
+			<div class="popup">
+				版本：1.0.0
+				开发者：292701515@qq.com
+			</div>
+		</van-popup>
 	</section>
 </template>
 
 <script>
-import { NavBar, Cell } from 'vant';
+import { NavBar, Cell, Popup } from 'vant';
 import { mapActions } from 'vuex';
 export default {
 	data() {
 		return {
-			userInfo: {}
+			userInfo: {},
+			showPopUp: false
 		};
 	},
 	components: {
 		[NavBar.name]: NavBar,
-		[Cell.name]: Cell
+		[Cell.name]: Cell,
+		[Popup.name]: Popup
 	},
 	created() {
 		this.init();
